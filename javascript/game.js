2839feec608ad9bb6f99c8b22b952cbdbf9b5844
Bruno Otávio <brunosantosprotec@gmail.com>
2019-04-16 14:41:00 -0300

    added some other things

--- javascript/game.js
+++ javascript/game.js
@@ -1,34 +1,65 @@
 let screen
 let battlefield
 let players = []
+let tanks = []
+let srcTank = []
 
-function initializeObjects(backOption, name1, name2){
+function initializeObjects(backOption, names, imgTank){
 	screen = createCanvas(900,600)
 
-	if (backOption == 1)
-	battlefield = createBattleField().withImage("images/background1.png")
-	if (backOption == 2)
-	battlefield = createBattleField().withImage("images/background2.png")
-	else
-	battlefield = createBattleField().withImage("images/background1.png")
+	setBackground(backOption)
+	
+	setTankImageSrc(imgTank, srcTank)
 
-	players.push(createPlayer().withName(name1).withLife(1000))
-	players.push(createPlayer().withName(name2).withLife(1000))
+	tanks.push(createTank().withImage(srcTank[0]))
+	tanks.push(createTank().withImage(srcTank[1]))
+
+	let index = 1
+	for(i in names){
+		if (!names[i]) {
+			names[i] = `player ${index}`
+		}
+		players.push(createPlayer().withName(names[i]).withLife(1000).withTank(tanks[i]))
+		index++
+	}
 }
 
-function gameStart(backOption, name1, name2){
+function gameStart(backOption, names, imgTank){
 
 	document.getElementById('players').hidden = false
 	document.getElementById('simpleForm').hidden = true
 
-	if (name1 && name2)
-	initializeObjects(backOption, name1, name2)
-	else
-	initializeObjects(backOption, 'player1', 'player2')
+	initializeObjects(backOption, names, imgTank)
 	
 	battlefield.background.onload = function(){
 		screen.ctx.drawImage(battlefield.background,0,0);   
 	}
 
 	updatePanelData(players)
+}
+
+function setTankImageSrc(imgTank, srcTank){
+
+	for(i in imgTank){
+		if (imgTank[i] == 1)
+			srcTank.push("images/tank1.png")
+		else if(imgTank[i] == 2)
+			srcTank.push("images/tank2.png")
+		else if(imgTank[i] == 3)
+			srcTank.push("images/tank3.png")
+		else if(imgTank[i] == 4)
+			srcTank.push("images/tank4.png")
+		else
+			srcTank.push("images/tank1.png")
+	}
+}
+
+function setBackground(backOption) {
+
+	if (backOption == 1)
+		battlefield = createBattleField().withImage("images/background1.png")
+	if (backOption == 2)
+		battlefield = createBattleField().withImage("images/background2.png")
+	else
+		battlefield = createBattleField().withImage("images/background1.png")
 }
